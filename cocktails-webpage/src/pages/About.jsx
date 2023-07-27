a3d09d600d0c48a31f773248adb5cdd8a7eebec0
MANISH <manish25250@gmail.com>
2023-07-27 09:54:09 +0530

    cocktail webiste v1

---
+++ cocktails-webpage/src/pages/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, deserunt
+        praesentium vitae ducimus, unde eos veniam excepturi, velit amet
+        voluptate corrupti inventore non asperiores! Doloribus, cupiditate
+        minima architecto id quas ad accusamus velit impedit, possimus,
+        recusandae modi deleniti ullam ea!
+      </p>
+    </section>
+  );
+};
+
+export default About;
